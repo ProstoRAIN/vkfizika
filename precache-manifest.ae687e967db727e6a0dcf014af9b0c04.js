a217b785b70ae861dfc00a9f0bee28cfa6346abc
ProstoRAIN <64376319+ProstoRAIN@users.noreply.github.com>
2020-07-11 02:40:35 +0300

    Updates

---
+++ precache-manifest.ae687e967db727e6a0dcf014af9b0c04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437a4c22d3abd84a5196ae69ed3d58eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b3afbe2990a986924d4",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "bf5d4cc4d5fffed4b40b",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2b3afbe2990a986924d4",
+    "url": "./static/js/2.a73712e7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.a73712e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5d4cc4d5fffed4b40b",
+    "url": "./static/js/main.7556c339.chunk.js"
+  },
+  {
+    "revision": "c900040a519d90f7a8cb",
+    "url": "./static/js/runtime-main.2e8b1c4e.js"
+  }
+]);
